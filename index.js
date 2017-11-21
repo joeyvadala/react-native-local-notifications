4d0559a130dc4e8fa33333e90be17d89698c3225
Joey Vadala <joey@joeyvadala.com>
2017-11-20 18:07:02 -0600

    Whoops, missed it in both places

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {NativeModules} from 'react-native';
 
 var RNLocalNotifications = {
-  createNotification: function(id, text, datetime,sound) {
+  createNotification: function(id, text, datetime, sound, repeatType) {
         NativeModules.RNLocalNotifications.createNotification(id, text, datetime, sound, repeatType);
   },
   deleteNotification: function(id) {
         NativeModules.RNLocalNotifications.deleteNotification(id);
   },
-  updateNotification: function(id, text, datetime, sound) {
+  updateNotification: function(id, text, datetime, sound, repeatType) {
         NativeModules.RNLocalNotifications.updateNotification(id, text, datetime, sound, repeatType);
   },
 };
