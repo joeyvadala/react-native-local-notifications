2532c477f7b502d6ba1b633800a17a39ed1d50c2
Joey Vadala <joey@joeyvadala.com>
2017-11-20 18:02:33 -0600

    Add repeatType param to index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import {NativeModules} from 'react-native';
 
 var RNLocalNotifications = {
   createNotification: function(id, text, datetime,sound) {
-        NativeModules.RNLocalNotifications.createNotification(id, text, datetime, sound);
+        NativeModules.RNLocalNotifications.createNotification(id, text, datetime, sound, repeatType);
   },
   deleteNotification: function(id) {
         NativeModules.RNLocalNotifications.deleteNotification(id);
   },
   updateNotification: function(id, text, datetime, sound) {
-        NativeModules.RNLocalNotifications.updateNotification(id, text, datetime, sound);
+        NativeModules.RNLocalNotifications.updateNotification(id, text, datetime, sound, repeatType);
   },
 };
 
