fb51712c7f404b85c2a693c154b4e10803222685
Joseph Vadala <joey@joeyvadala.com>
2018-02-26 11:54:30 -0600

    Testing revert

--- ios/RNLocalNotifications/RNLocalNotifications.m
+++ ios/RNLocalNotifications/RNLocalNotifications.m
@@ -28,74 +28,74 @@ RCT_EXPORT_METHOD(updateNotification:(NSInteger *)id text:(NSString *)text datet
     if (update) {
         [self deleteAlarm:id];
     }
-    
-    // Rewritten to use UNUserNotificationCenter by Joseph Vadala 2.26.18
-    
-    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-    UNAuthorizationOptions options = UNAuthorizationOptionAlert + UNAuthorizationOptionSound;
-
-    [center requestAuthorizationWithOptions:options
-                          completionHandler:^(BOOL granted, NSError * _Nullable error) {
-                              if (!granted) {
-                                  NSLog(@"Something went wrong");
-                              }
-                          }];
-    
-    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
-    content.title = @"She Reads Truth";
-    content.body = text;
-    content.sound = [UNNotificationSound defaultSound];
+//
+//    // Rewritten to use UNUserNotificationCenter by Joseph Vadala 2.26.18
+//
+//    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+//    UNAuthorizationOptions options = UNAuthorizationOptionAlert + UNAuthorizationOptionSound;
+//
+//    [center requestAuthorizationWithOptions:options
+//                          completionHandler:^(BOOL granted, NSError * _Nullable error) {
+//                              if (!granted) {
+//                                  NSLog(@"Something went wrong");
+//                              }
+//                          }];
+//
+//    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+//    content.title = @"She Reads Truth";
+//    content.body = text;
+//    content.sound = [UNNotificationSound defaultSound];
+//
+//    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+//    [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm"];
+//    NSDate *fireDate = [dateFormat dateFromString:datetime];
+//
+//    // Temp hourly repeat for testing
+//    // NSDateComponents *triggerDate = [[NSCalendar currentCalendar] components:NSCalendarUnitHour + NSCalendarUnitMinute + NSCalendarUnitSecond fromDate:fireDate];
+//    NSDateComponents *triggerDate = [[NSCalendar currentCalendar] components:NSCalendarUnitMinute + NSCalendarUnitSecond fromDate:fireDate];
+//
+//    UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:triggerDate repeats:YES];
+//
+//    NSString *identifier = @"SRTLocalNotification";
+//    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+//
+//    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+//        if (error != nil) {
+//            NSLog(@"Something went wrong: %@", error);
+//        }
+//    }];
     
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm"];
     NSDate *fireDate = [dateFormat dateFromString:datetime];
-    
-    // Temp hourly repeat for testing
-    // NSDateComponents *triggerDate = [[NSCalendar currentCalendar] components:NSCalendarUnitHour + NSCalendarUnitMinute + NSCalendarUnitSecond fromDate:fireDate];
-    NSDateComponents *triggerDate = [[NSCalendar currentCalendar] components:NSCalendarUnitMinute + NSCalendarUnitSecond fromDate:fireDate];
-    
-    UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:triggerDate repeats:YES];
-    
-    NSString *identifier = @"SRTLocalNotification";
-    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
-    
-    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
-        if (error != nil) {
-            NSLog(@"Something went wrong: %@", error);
+    if ([[NSDate date]compare: fireDate] == NSOrderedAscending) { //only schedule items in the future!
+        UILocalNotification *notification = [[UILocalNotification alloc] init];
+        notification.fireDate = fireDate;
+        if(![sound isEqualToString:@""] && ![sound isEqualToString:@"silence"]){
+            notification.soundName = @"alarm.caf";
         }
-    }];
-    
-//    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-//    [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm"];
-//    NSDate *fireDate = [dateFormat dateFromString:datetime];
-//    if ([[NSDate date]compare: fireDate] == NSOrderedAscending) { //only schedule items in the future!
-//        UILocalNotification *notification = [[UILocalNotification alloc] init];
-//        notification.fireDate = fireDate;
-//        if(![sound isEqualToString:@""] && ![sound isEqualToString:@"silence"]){
-//            notification.soundName = @"alarm.caf";
-//        }
-//        else {
-//            notification.soundName = @"silence.caf";
-//        }
-//        notification.timeZone = [NSTimeZone defaultTimeZone];
-//        notification.alertBody = text;
-//        notification.alertAction = @"Open";
-//
-//        // Added by Joseph W Vadala on 11/20/2017
-//        // Made this the default because we need to make this work
-//        // Temp change to Minute to test
-//        notification.repeatInterval = NSCalendarUnitMinute;
-//
-//        int a = ((int)[[UIApplication sharedApplication] applicationIconBadgeNumber] + 1);
-//        notification.applicationIconBadgeNumber = a;
-//        NSMutableDictionary *md = [[NSMutableDictionary alloc] init];
-//        [md setValue:[NSNumber numberWithInteger:id] forKey:@"id"];
-//        [md setValue:text forKey:@"text"];
-//        [md setValue:datetime forKey:@"datetime"];
-//        [md setValue:sound forKey:@"sound"];
-//        notification.userInfo = md;
-//        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
-//    }
+        else {
+            notification.soundName = @"silence.caf";
+        }
+        notification.timeZone = [NSTimeZone defaultTimeZone];
+        notification.alertBody = text;
+        notification.alertAction = @"Open";
+
+        // Added by Joseph W Vadala on 11/20/2017
+        // Made this the default because we need to make this work
+        // Temp change to Minute to test
+        notification.repeatInterval = NSCalendarUnitMinute;
+
+        int a = ((int)[[UIApplication sharedApplication] applicationIconBadgeNumber] + 1);
+        notification.applicationIconBadgeNumber = a;
+        NSMutableDictionary *md = [[NSMutableDictionary alloc] init];
+        [md setValue:[NSNumber numberWithInteger:id] forKey:@"id"];
+        [md setValue:text forKey:@"text"];
+        [md setValue:datetime forKey:@"datetime"];
+        [md setValue:sound forKey:@"sound"];
+        notification.userInfo = md;
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    }
 }
 
 - (void)deleteAlarm:(NSInteger *)id {
